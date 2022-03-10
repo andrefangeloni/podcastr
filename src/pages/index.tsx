4b537351c30165b2be5edac2ceb76fda086a910b
André Angeloni <andrefangeloni@gmail.com>
2022-03-09 22:30:11 -0300

    feat: SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,16 @@
-const Home = () => null;
+import api from '../services/api';
+
+const Home = ({ episodes }) => <p>{JSON.stringify(episodes)}</p>;
 
 export default Home;
+
+export const getStaticProps = async () => {
+  const { data } = await api.get('/episodes');
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
+};
