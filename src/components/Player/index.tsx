c2f0cf1049833a7844a10a083ca2dbb6a9ba318c
André Angeloni <andrefangeloni@gmail.com>
2022-03-12 23:14:25 -0300

    feat: styled conditional buttons

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 import Image from 'next/image';
 
+import Slider from 'rc-slider';
+
 import { PlayerContext } from '../../contexts/PlayerContext';
 
+import 'rc-slider/assets/index.css';
 import styles from './styles.module.scss';
 
 const Player = () => {
@@ -40,29 +43,37 @@ const Player = () => {
         <div className={styles.progress}>
           <span>00:00</span>
           <div className={styles.slider}>
-            <div className={styles.emptySlider} />
+            {episode ? (
+              <Slider
+                railStyle={{ backgroundColor: '#9f75ff' }}
+                trackStyle={{ backgroundColor: '#04d361' }}
+                handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
+              />
+            ) : (
+              <div className={styles.emptySlider} />
+            )}
           </div>
           <span>00:00</span>
         </div>
 
         <div className={styles.buttons}>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Embaralhar" />
           </button>
 
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt="Tocar anterior" />
           </button>
 
-          <button type="button" className={styles.playButton}>
+          <button type="button" disabled={!episode} className={styles.playButton}>
             <img src="/play.svg" alt="Tocar" />
           </button>
 
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-next.svg" alt="Tocar próxima" />
           </button>
 
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/repeat.svg" alt="Repetir" />
           </button>
         </div>
